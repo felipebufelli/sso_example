9b41cbee715980c54c5ecbd0b53cbcb645c26a7a
Felipe Bufelli <felipe.bufelli@gmail.com>
2021-08-02 16:55:15 -0300

    Tentativa 1

--- lib/app/features/autentication/controller/autentication_controller.dart
+++ lib/app/features/autentication/controller/autentication_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 enum AutenticationState { initial, loading, error, sucess }
 
@@ -13,10 +14,22 @@ class AutenticationController extends ChangeNotifier {
 
   AutenticationState get state => _state;
 
+  String identityProviderURL =
+      "https://dev-43408951.okta.com/app/dev-43408951_estudosamlsso_1/exk1e8wfgtEGIt6iI5d7/sso/saml";
+  String identityProviderIssuer = "http://www.okta.com/exk1e8wfgtEGIt6iI5d7";
+  String certificate =
+      "MIIDqDCCApCgAwIBAgIGAXsH0FUgMA0GCSqGSIb3DQEBCwUAMIGUMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEUMBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGRldi00MzQwODk1MTEcMBoGCSqGSIb3DQEJARYNaW5mb0Bva3RhLmNvbTAeFw0yMTA4MDIxNzAwMzhaFw0zMTA4MDIxNzAxMzhaMIGUMQswCQYDVQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEUMBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGRldi00MzQwODk1MTEcMBoGCSqGSIb3DQEJARYNaW5mb0Bva3RhLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAIaRmcoV/ookR9X0R1n0H1X9FIfEFo11r8b63ybrMxYjdeQq8D5ZgH9dOaNnApYZtpldvCTYm7zL14CaJDBz/YecLMgHweBe/+JN6hW2xp1siamaLgQgxatNYekX+ilDlo8Cy2W3JxejgnAJ9Qwn3/bkerjGkaC+mSUprsTFQvLAcKnkQYsz8jur8qplvZdEia/M3OvjN8c+UVOmPvre6iYxeeDAa+H1BVlU6HwHSLxVDnWYdzF4dtywlYL2F9WstX86ihawWX5xvzjGpC82DTYHx9g/8aegS+6E1WGOw9Ten4ZEZBYKSuqK+Y7X5P+bL9h6K7o+6yC+M9EANr1yhNUCAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAQcPJnLgqF7yBNRJFey7LG9qRYjRNDDiOqTfLFAfJj8bQd3WHmDZ6lajwOPCtfIbsrGlrfGcY/7SJrsDcHHj6SIe1+AgwPkIy0nxSL++iJ9LOixc1qMCl9jSIi5Thk5NYgWCpLQSINSGEN7Ted81T4c8QCynjrOg0iaoNYoZQoTmtqOzSXvKt/k1Hy18Dr+KlkmM/8PqkxTP4QFQ3VypXKU+dfQQrxfSMLG4aes4DqhRQzxK650KdxrAuZBApFGklSThFHDaR2gfHt7/NwN9JlOicCtePtlUjEOxKf0jnsGNG3yCOQEpRUZbTR4umEn0bSxgTgAcjx8W9evPeHNMCMg=="
+      "";
+
   Future<void> authenticate() async {
     try {
       state = AutenticationState.loading;
-      await Future.delayed(const Duration(seconds: 2));
+      final http.Response response = await http.get(
+        Uri.parse(
+          identityProviderURL,
+        ),
+      );
+      debugPrint(response.body);
       state = AutenticationState.sucess;
     } catch (e) {
       debugPrint("Autentication Error: $e");
--- lib/app/features/autentication/view/autentication_page.dart
+++ lib/app/features/autentication/view/autentication_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:provider/provider.dart';
 import 'package:sso_example/app/features/autentication/controller/autentication_controller.dart';
 
@@ -17,6 +18,16 @@ class AutenticationPage extends StatelessWidget {
                   await autenticationController.authenticate();
                 },
                 child: Text("Login with SSO"),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 32,
+                ),
+                child: Html(
+                  data: """<script src="https://global.oktacdn.com/okta-signin-widget/5.9.0/js/okta-sign-in.min.js" type="text/javascript"></script>
+                  <link href="https://global.oktacdn.com/okta-signin-widget/5.9.0/css/okta-sign-in.min.css" type="text/css" rel="stylesheet"/> 
+                  """,
+                ),
               ),
               if (autenticationController.state ==
                   AutenticationState.loading) ...[
